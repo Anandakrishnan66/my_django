def04bb670dc8ccb83faff25b68e19ae3fb12fb9
Anandakrishnan66 <ananda2000krishnan@gmail.com>
2023-07-27 15:55:24 +0530

    first

---
+++ Log_Django/logApp/logApp/info.py
@@ -0,0 +1,8 @@
+EMAIL_USE_TLS = True
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_HOST_USER="ananda33krishnan@gmail.com"
+EMAIL_HOST_PASSWORD="forcaurwwfpfeqts"
+EMAIL_PORT=587
+
+
+#logApp.demo.django.login@gmail.com
