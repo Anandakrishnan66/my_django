1a9e1633c110c43f0c8552c43e9e40a5b75de066
AnandaKrishnan <85782904+Anandakrishnan66@users.noreply.github.com>
2023-07-27 15:56:43 +0530

    Update

--- Log_Django/logApp/logApp/info.py
+++ Log_Django/logApp/logApp/info.py
@@ -1,8 +1,4 @@
-EMAIL_USE_TLS = True
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER="ananda33krishnan@gmail.com"
-EMAIL_HOST_PASSWORD="forcaurwwfpfeqts"
-EMAIL_PORT=587
+
 
 
 #logApp.demo.django.login@gmail.com
